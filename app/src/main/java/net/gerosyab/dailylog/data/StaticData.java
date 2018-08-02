ba968942be8f5da4d42fd0490e24c224f92f12e8
Donghe Youn <gerosyab@gmail.com>
2018-08-02 17:31:05 +0900

    add some constants

--- app/src/main/java/net/gerosyab/dailylog/data/StaticData.java
+++ app/src/main/java/net/gerosyab/dailylog/data/StaticData.java
@@ -10,10 +10,11 @@ import java.text.SimpleDateFormat;
  */
 public class StaticData {
     public static final String CATEGORY_MODE_INTENT_EXTRA = "Intent Extra for Category Mode";
-    public static final String CATEGORY_NUM_INTENT_EXTRA = "Intent Extra for Category Num";
-    public static final String CATEGORY_NAME_INTENT_EXTRA = "Intent Extra for Category Name";
-    public static final String CATEGORY_ORDER_INTENT_EXTRA = "Intent Extra for Category Order";
+//    public static final String CATEGORY_NUM_INTENT_EXTRA = "Intent Extra for Category Num";
+//    public static final String CATEGORY_NAME_INTENT_EXTRA = "Intent Extra for Category Name";
+//    public static final String CATEGORY_ORDER_INTENT_EXTRA = "Intent Extra for Category Order";
     public static final String CATEGORY_ID_INTENT_EXTRA = "Intent Extra for Category IDr";
+    public static final String RAW_TEXT_INTENT_EXTRA = "Intent Extra for Raw Text";
     public static final long CATEGORY_MODE_CREATE = 0;
     public static final long CATEGORY_MODE_EDIT = 1;
     public static final long DIALOG_MODE_CREATE = 0;
@@ -22,6 +23,9 @@ public class StaticData {
     public static final long RECORD_TYPE_BOOLEAN = 0;
     public static final long RECORD_TYPE_NUMBER = 1;
     public static final long RECORD_TYPE_MEMO = 2;
+//    public static final long chartXAxisWeight = 10000000;
+
+    public static final String[] RECORD_TYPE_NAME = {"Boolean", "Number", "Memo"};
 
     public static final DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd");
     public static final DateTimeFormatter fmtForBackup = DateTimeFormat.forPattern("yyyy-MM-dd 24HH:mm:ss");
